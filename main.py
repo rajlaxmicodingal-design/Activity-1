c79468147134d9de6b0da0357b45e48804279f99
rajlaxmicodingal-design <rajlaxmicodingal@gmail.com>
2026-01-08 08:55:44 +0530

    lesson1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+#Printing a welcome message
 print("HELLO, Welcome to Codingal!!!")
+
+#Printing my name
+print("My name is John Doe.")
+
+#Printing my age
+print("I am 20 years old.")
+
+#Printing a simple arithmetic operation
+print("The sum of 5 and 3 is:", 5 + 3)
